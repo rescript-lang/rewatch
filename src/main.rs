22d182d73f390f84c594617219495c024dbcb585
Roland Peelen <roland@peppr.nl>
2023-01-12 23:55:11 +0200

    :art: - Add walnut as submodule

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,22 @@ pub mod structure;
 pub mod watcher;
 
 fn main() {
-    //let structure = structure::read_structure("dir");
+    let structure = structure::read_structure("walnut");
 
-    //match structure {
-    //Ok(s) => println!("{}", s),
-    //Err(_) => println!("Could not read fs"),
-    //}
-    //
-    let root_bs_config = bsconfig::read("dir/bsconfig.json".to_string());
+    match structure {
+        Ok(s) => println!("{}", s),
+        Err(_) => println!("Could not read fs"),
+    }
+    let root_bs_config = bsconfig::read("walnut/bsconfig.json".to_string());
     let _ = root_bs_config
         .pinned_dependencies
         .unwrap_or(vec![])
         .iter()
-        .map(|dep| bsconfig::read("dir/node_modules/".to_string() + dep + "/bsconfig.json"))
+        .map(|dep| bsconfig::read("walnut/node_modules/".to_string() + dep + "/bsconfig.json"))
         .for_each(|config| println!("{config:?}"));
 
     futures::executor::block_on(async {
-        if let Err(e) = watcher::async_watch("dir").await {
+        if let Err(e) = watcher::async_watch("walnut").await {
             println!("error: {:?}", e)
         }
     });
