32b50741bd65766b137e6ed795dc3d5bf800e974
Roland Peelen <roland@peppr.nl>
2023-01-13 08:33:48 +0200

    :bug: - Fix dir

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,22 @@ pub mod structure;
 pub mod watcher;
 
 fn main() {
-    let structure = structure::read_structure("walnut");
+    let structure = structure::read_structure("walnut_monorepo");
 
     match structure {
         Ok(s) => println!("{}", s),
         Err(_) => println!("Could not read fs"),
     }
-    let root_bs_config = bsconfig::read("walnut/bsconfig.json".to_string());
+    let root_bs_config = bsconfig::read("walnut_monorepo/bsconfig.json".to_string());
     let _ = root_bs_config
         .pinned_dependencies
         .unwrap_or(vec![])
         .iter()
-        .map(|dep| bsconfig::read("walnut/node_modules/".to_string() + dep + "/bsconfig.json"))
+        .map(|dep| bsconfig::read("walnut_monorepo/node_modules/".to_string() + dep + "/bsconfig.json"))
         .for_each(|config| println!("{config:?}"));
 
     futures::executor::block_on(async {
-        if let Err(e) = watcher::async_watch("walnut").await {
+        if let Err(e) = watcher::async_watch("walnut_monorepo").await {
             println!("error: {:?}", e)
         }
     });
