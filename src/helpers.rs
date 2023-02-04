5d17840cc2deca34766706c0a1d73c80ded8618a
Roland Peelen <roland@peppr.nl>
2023-02-04 15:35:14 +0100

    :poop: - Sync

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,6 +1,22 @@
 use std::fs;
 use std::path::PathBuf;
 
+pub fn get_package_path(root: &str, package_name: &str) -> String {
+    format!("{}/node_modules/{}", root, package_name)
+}
+
+pub fn get_build_path(root: &str, package_name: &str) -> String {
+    format!("{}/node_modules/{}/_build", root, package_name)
+}
+
+pub fn get_path(root: &str, package_name: &str, file: &str) -> String {
+    format!("{}/{}/{}", root, package_name, file)
+}
+
+pub fn get_node_modules_path(root: &str) -> String {
+    format!("{}/node_modules", root)
+}
+
 pub fn get_abs_path(path: &str) -> String {
     let abs_path_buf = PathBuf::from(path);
     return fs::canonicalize(abs_path_buf)
