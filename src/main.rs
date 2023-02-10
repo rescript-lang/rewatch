cfbdd88e4f2d2ed0fb04389ccb3b20528ba1fb52
Roland Peelen <roland@peppr.nl>
2023-02-10 15:33:50 +0100

    :zap: - Parallel Compilation

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ pub mod structure_hashmap;
 pub mod watcher;
 use ahash::AHashSet;
 use helpers::*;
+use rayon::prelude::*;
 
 fn main() {
     let project_root = helpers::get_abs_path("walnut_monorepo");
@@ -28,56 +29,67 @@ fn main() {
     loop {
         dbg!("COMPILE PASS");
         let mut compiled_count = 0;
-        for (module_name, module) in modules.iter() {
-            if module.deps.is_subset(&compiled_modules) && !compiled_modules.contains(module_name) {
-                compiled_count += 1;
-                match module.source_type.to_owned() {
-                    build::SourceType::MlMap => {
-                        // build::compile_mlmap(&module.package, &module_name, &project_root);
-                        let _ = compiled_modules.insert(module_name.to_owned());
-                    }
-                    build::SourceType::SourceFile => {
-                        // compile interface first
-                        dbg!("Compiling ".to_string() + &module_name);
-                        match module.asti_path.to_owned() {
-                            Some(asti_path) => {
-                                dbg!("HAS INTERFACE");
-                                build::compile_file(
-                                    &get_package_path(&project_root, &module.package.name),
-                                    &get_node_modules_path(&project_root),
-                                    &asti_path,
-                                    module,
-                                    true,
-                                );
-                            }
-                            _ => {
-                                dbg!("NO INTERFACE");
-                                ();
-                            }
+        modules
+            .par_iter()
+            .map(|(module_name, module)| {
+                if module.deps.is_subset(&compiled_modules)
+                    && !compiled_modules.contains(module_name)
+                {
+                    match module.source_type.to_owned() {
+                        build::SourceType::MlMap => {
+                            // build::compile_mlmap(&module.package, &module_name, &project_root);
+                            Some(module_name.to_owned())
                         }
+                        build::SourceType::SourceFile => {
+                            // compile interface first
+                            dbg!("Compiling ".to_string() + &module_name);
+                            match module.asti_path.to_owned() {
+                                Some(asti_path) => {
+                                    dbg!("HAS INTERFACE");
+                                    build::compile_file(
+                                        &get_package_path(&project_root, &module.package.name),
+                                        &get_node_modules_path(&project_root),
+                                        &asti_path,
+                                        module,
+                                        true,
+                                    );
+                                }
+                                _ => {
+                                    dbg!("NO INTERFACE");
+                                    ();
+                                }
+                            }
 
-                        build::compile_file(
-                            &get_package_path(&project_root, &module.package.name),
-                            &get_node_modules_path(&project_root),
-                            &module.ast_path.to_owned().unwrap(),
-                            module,
-                            false,
-                        );
-                        let _ = compiled_modules.insert(module_name.to_owned());
+                            build::compile_file(
+                                &get_package_path(&project_root, &module.package.name),
+                                &get_node_modules_path(&project_root),
+                                &module.ast_path.to_owned().unwrap(),
+                                module,
+                                false,
+                            );
+                            Some(module_name.to_owned())
+                        }
                     }
+                } else if !compiled_modules.contains(module_name) {
+                    dbg!(format!("Still uncompiled deps for: {}", module_name));
+                    None
+                } else {
+                    None
                 }
-            } else if !compiled_modules.contains(module_name) {
-                dbg!(format!("Still uncompiled deps for: {}", module_name));
-                dbg!(module
-                    .deps
-                    .difference(&compiled_modules)
-                    .collect::<Vec<&String>>());
-            }
-        }
+            })
+            .collect::<Vec<Option<String>>>()
+            .iter()
+            .for_each(|module_name| {
+                module_name.iter().for_each(|name| {
+                    compiled_count += 1;
+                    compiled_modules.insert(name.to_string());
+                })
+            });
+
         if compiled_count == 0 {
-            dbg!(all_modules
-                .difference(&compiled_modules)
-                .collect::<Vec<&String>>());
+            //dbg!(all_modules
+            //.difference(&compiled_modules)
+            //.collect::<Vec<&String>>());
             dbg!("No incremental compile -- breaking");
             break;
         };
