667c0438b8e948d18dc0a256e3ac3344b0ff58fc
Roland Peelen <roland@peppr.nl>
2023-03-06 11:25:57 +0100

    :zap: - Include file-change in build benchmark

--- benches/base_bench.rs
+++ benches/base_bench.rs
@@ -1,11 +1,40 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use rewatch::build;
 
+use std::fs::File;
+use std::io::prelude::*;
+
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("clean-build", |b| {
+    c.bench_function("clean-build-change-build", |b| {
+        // Folder for the testrepo
+        let folder = "testrepo";
+        let filename = "testrepo/packages/dep02/src/Dep02.res";
+        // Clean the build
+        build::clean(folder);
+        // Read the file we'll be mutating
+        let mut file = File::options()
+            .read(true)
+            .write(true)
+            .append(true)
+            .open(filename)
+            .unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+
         b.iter(|| {
-            let folder = "testrepo";
-            build::clean(folder);
+            // Create initial build
+            let _ = build::build(folder);
+            // Update the file
+            let _ = writeln!(
+                file,
+                r#"let log2 = () => ["a", "b"]->forEach(Js.log);log2()"#
+            );
+            // Create another build
+            let _ = build::build(folder);
+
+            // Reset state
+            File::create(filename).unwrap();
+            file.write_all(contents.as_bytes()).unwrap();
             let _ = build::build(folder);
         })
     });
