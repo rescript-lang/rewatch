18232903104c20a43f387c33698e87ae8a6fa4c4
Jaap Frolich <jfrolich@gmail.com>
2024-04-03 09:34:40 +0200

    :bug: - fix errors in base bench

--- benches/base_bench.rs
+++ benches/base_bench.rs
@@ -1,6 +1,7 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use rewatch::build;
 use rewatch::build::packages;
+use rewatch::clean;
 use rewatch::helpers;
 
 use std::fs::File;
@@ -13,7 +14,7 @@ fn criterion_benchmark(c: &mut Criterion) {
         let project_root = helpers::get_abs_path(folder);
 
         b.iter(|| {
-            packages::make(&project_root);
+            packages::make(&None, &project_root, &None);
         })
     });
 
@@ -22,7 +23,7 @@ fn criterion_benchmark(c: &mut Criterion) {
         let folder = "testrepo";
         let filename = "testrepo/packages/dep02/src/Dep02.res";
         // Clean the build
-        build::clean(folder);
+        clean::clean(folder);
         // Read the file we'll be mutating
         let mut file = File::options()
             .read(true)
@@ -35,16 +36,16 @@ fn criterion_benchmark(c: &mut Criterion) {
 
         b.iter(|| {
             // Create initial build
-            let _ = build::build(folder);
+            let _ = build::build(&None, folder, false);
             // Update the file
             let _ = writeln!(file, r#"let log2 = () => ["a", "b"]->forEach(Js.log);log2()"#);
             // Create another build
-            let _ = build::build(folder);
+            let _ = build::build(&None, folder, false);
 
             // Reset state
             File::create(filename).unwrap();
             file.write_all(contents.as_bytes()).unwrap();
-            let _ = build::build(folder);
+            let _ = build::build(&None, folder, false);
         })
     });
 }
