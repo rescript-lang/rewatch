fc8abeb05ba34cf4a781d22a696306a957712e1a
nojaf <florian.verdonck@outlook.com>
2025-05-03 12:35:40 +0200

    24

--- tests/suffix.sh
+++ tests/suffix.sh
@@ -27,7 +27,7 @@ fi
 # Count files with new extension
 file_count=$(find . -name *.res.js | wc -l)
 
-if [ "$file_count" -eq 12 ];
+if [ "$file_count" -eq 24 ];
 then
   success "Found files with correct suffix"
 else
