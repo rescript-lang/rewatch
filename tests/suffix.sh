252c51fc75a46f4d1cb0065f3681406a1f883ea5
nojaf <florian.verdonck@outlook.com>
2025-05-03 12:17:07 +0200

    Try 12

--- tests/suffix.sh
+++ tests/suffix.sh
@@ -27,7 +27,7 @@ fi
 # Count files with new extension
 file_count=$(find . -name *.res.js | wc -l)
 
-if [ "$file_count" -eq 22 ];
+if [ "$file_count" -eq 12 ];
 then
   success "Found files with correct suffix"
 else
