7f3a4628c5d626d51c77deb706246211623f9eaa
Bushuo <me@paulbuschmann.com>
2025-05-04 16:50:53 +0200

    test: improve testsuite and dev deps test case

--- tests/compile.sh
+++ tests/compile.sh
@@ -33,36 +33,36 @@ fi
 node ./packages/main/src/Main.mjs > ./packages/main/src/output.txt
 
 mv ./packages/main/src/Main.res ./packages/main/src/Main2.res
-rewatch build --no-timing=true &> ../tests/snapshots/rename-file.txt
+rewatch build &> ../tests/snapshots/rename-file.txt
 mv ./packages/main/src/Main2.res ./packages/main/src/Main.res
 
 # Rename a file with a dependent - this should trigger an error
 mv ./packages/main/src/InternalDep.res ./packages/main/src/InternalDep2.res
-rewatch build --no-timing=true &> ../tests/snapshots/rename-file-internal-dep.txt
+rewatch build &> ../tests/snapshots/rename-file-internal-dep.txt
 # replace the absolute path so the snapshot is the same on all machines
 replace "s/$(pwd | sed "s/\//\\\\\//g")//g" ../tests/snapshots/rename-file-internal-dep.txt
 mv ./packages/main/src/InternalDep2.res ./packages/main/src/InternalDep.res
 
 # Rename a file with a dependent in a namespaced package - this should trigger an error (regression)
 mv ./packages/new-namespace/src/Other_module.res ./packages/new-namespace/src/Other_module2.res
-rewatch build --no-timing=true &> ../tests/snapshots/rename-file-internal-dep-namespace.txt
+rewatch build &> ../tests/snapshots/rename-file-internal-dep-namespace.txt
 # replace the absolute path so the snapshot is the same on all machines
 replace "s/$(pwd | sed "s/\//\\\\\//g")//g" ../tests/snapshots/rename-file-internal-dep-namespace.txt
 mv ./packages/new-namespace/src/Other_module2.res ./packages/new-namespace/src/Other_module.res
 
 rewatch build &>  /dev/null
 mv ./packages/main/src/ModuleWithInterface.resi ./packages/main/src/ModuleWithInterface2.resi
-rewatch build --no-timing=true &> ../tests/snapshots/rename-interface-file.txt
+rewatch build &> ../tests/snapshots/rename-interface-file.txt
 mv ./packages/main/src/ModuleWithInterface2.resi ./packages/main/src/ModuleWithInterface.resi
 rewatch build &> /dev/null
 mv ./packages/main/src/ModuleWithInterface.res ./packages/main/src/ModuleWithInterface2.res
-rewatch build --no-timing=true &> ../tests/snapshots/rename-file-with-interface.txt
+rewatch build &> ../tests/snapshots/rename-file-with-interface.txt
 mv ./packages/main/src/ModuleWithInterface2.res ./packages/main/src/ModuleWithInterface.res
 rewatch build &> /dev/null
 
 # when deleting a file that other files depend on, the compile should fail
 rm packages/dep02/src/Dep02.res
-rewatch build --no-timing=true &> ../tests/snapshots/remove-file.txt
+rewatch build &> ../tests/snapshots/remove-file.txt
 # replace the absolute path so the snapshot is the same on all machines
 replace "s/$(pwd | sed "s/\//\\\\\//g")//g" ../tests/snapshots/remove-file.txt
 git checkout -- packages/dep02/src/Dep02.res
@@ -70,14 +70,20 @@ rewatch build &> /dev/null
 
 # it should show an error when we have a dependency cycle
 echo 'Dep01.log()' >> packages/new-namespace/src/NS_alias.res
-rewatch build --no-timing=true &> ../tests/snapshots/dependency-cycle.txt
+rewatch build &> ../tests/snapshots/dependency-cycle.txt
 git checkout -- packages/new-namespace/src/NS_alias.res
-rewatch build &> /dev/null
 
 # it should compile dev dependencies with the --dev flag
-rewatch build --dev &> /dev/null
-file_count=$(find ./packages/with-dev-deps -name *.mjs | wc -l)
-if [ "$file_count" -eq 2 ];
+rewatch clean &> /dev/null
+rewatch build --dev &> /dev/null;
+if [ $? -ne 0 ];
+then
+  error "Failed to compile dev dependencies"
+  exit 1
+fi
+
+file_count=$(find ./packages/with-dev-deps/test -name *.mjs | wc -l)
+if [ "$file_count" -eq 1 ];
 then
   success "Compiled dev dependencies successfully"
 else
